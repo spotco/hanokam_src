12aa19560b5c39187c28ff492a1c727e756a2fea
Kenneth Pu <kp295@cornell.edu>
2015-06-06 11:15:25 -0700

    Only update villager state if player is on ground

--- Source/Classes/game/bg/elements/BGCharacterBase.m
+++ Source/Classes/game/bg/elements/BGCharacterBase.m
@@ -23,42 +23,46 @@ static CGFloat const TAP_RANGE = 50;
 @synthesize dialogueOffset = _dialogueOffset;
 
 -(void)i_update:(GameEngineScene*)g {
-    switch (self.state) {
-        case BGCharacter_Idle:;
-            // Proceed to CAN_SPEAK state if player is within range
-            CGFloat x1 = [g.player convertToWorldSpace:CGPointZero].x;
-            CGFloat x2 = [self convertToWorldSpace:CGPointZero].x;
-            if (abs(x1 - x2) < DIALOGUE_RANGE) {
-                _state = BGCharacter_CanSpeak;
-            }
-            break;
-        case BGCharacter_CanSpeak:
-            // If tapped, proceed to SPEAKING state, move player to IN_DIALOGUE state
-            if (g.get_control_manager.is_proc_tap) {
-                CGPoint p1 = g.get_control_manager.get_proc_tap;
-                CGPoint p2 = [self convertToWorldSpace:CGPointZero];
-                if (CGPointDist(p1, p2) < TAP_RANGE) {
-                    _state = BGCharacter_Speaking;
-                    g._player_state = PlayerState_InDialogue;
-                    break;
+    if (g.get_player_state == PlayerState_OnGround) {
+        switch (self.state) {
+            case BGCharacter_Idle:;
+                // Proceed to CAN_SPEAK state if player is within range
+                CGFloat x1 = [g.player convertToWorldSpace:CGPointZero].x;
+                CGFloat x2 = [self convertToWorldSpace:CGPointZero].x;
+                if (abs(x1 - x2) < DIALOGUE_RANGE) {
+                    _state = BGCharacter_CanSpeak;
                 }
-            }
-            // If player leaves range move to IDLE state
-            x1 = [g.player convertToWorldSpace:CGPointZero].x;
-            x2 = [self convertToWorldSpace:CGPointZero].x;
-            if (abs(x1 - x2) > DIALOGUE_RANGE) {
-                _state = BGCharacter_Idle;
-            }
-            break;
-        case BGCharacter_Speaking:
-            NSLog(@"SUP BRO");
-            // Once dialogue dismissed, move to CAN_SPEAK state, move player back to ON_GROUND state
-            _state = BGCharacter_CanSpeak;
-            g._player_state = PlayerState_OnGround;
-            break;
-        default:
-            break;
+                break;
+            case BGCharacter_CanSpeak:
+                // If tapped, proceed to SPEAKING state, move player to IN_DIALOGUE state
+                if (g.get_control_manager.is_proc_tap) {
+                    CGPoint p1 = g.get_control_manager.get_proc_tap;
+                    CGPoint p2 = [self convertToWorldSpace:CGPointZero];
+                    if (CGPointDist(p1, p2) < TAP_RANGE) {
+                        _state = BGCharacter_Speaking;
+    //                    g._player_state = PlayerState_InDialogue;
+                        break;
+                    }
+                }
+                // If player leaves range move to IDLE state
+                x1 = [g.player convertToWorldSpace:CGPointZero].x;
+                x2 = [self convertToWorldSpace:CGPointZero].x;
+                if (abs(x1 - x2) > DIALOGUE_RANGE) {
+                    _state = BGCharacter_Idle;
+                }
+                break;
+            case BGCharacter_Speaking:
+                NSLog(@"SUP BRO");
+                // Once dialogue dismissed, move to CAN_SPEAK state, move player back to ON_GROUND state
+                _state = BGCharacter_CanSpeak;
+    //            g._player_state = PlayerState_OnGround;
+                break;
+            default:
+                break;
+        }
+    } else {
+        _state = BGCharacter_Idle;
     }
 }
-    
+
 @end
